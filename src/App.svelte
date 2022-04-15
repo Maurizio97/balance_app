bc7e2edf3f8140d731bede581f2fcdcf682be103
Maurizio Proietto <87990939+Maurizio97@users.noreply.github.com>
2022-04-15 22:05:40 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	let total    = 0;
+	let gain     = 0;
+	let expenses = 0;
+</script>
+
+<main>
+	<h1>Totale {total}</h1>
+	<h1>Spese {gain}</h1>
+	<h1>Guadagni {expenses}</h1>
+</main>
+
+<style>
+</style>
